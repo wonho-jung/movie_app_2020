6a5dee69acee5fa6451d4e0a68c1e9627c3970bd
wonho-jung <wonho.jung92@gmail.com>
2020-07-31 15:12:10 -0400

    Updates

---
+++ precache-manifest.022a39ddf203ecc884d72f0fb800b6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82db8003c6623da808df9b211a623af",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d3983c2638b5ee270380",
+    "url": "/movie_app_2020/static/css/main.4e7dfbc7.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3983c2638b5ee270380",
+    "url": "/movie_app_2020/static/js/main.452be26d.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
